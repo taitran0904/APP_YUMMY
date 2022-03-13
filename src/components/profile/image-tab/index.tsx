123f31100d9c27ea9360aad11aed2ce8c8391297
@taitran0904 <1824801030018@student.tdmu.edu.vn>
2022-03-13 22:35:23 +0700

    set tab

---
+++ src/components/profile/image-tab/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Block, Text } from "../../../helper";
+const ImageTab: React.FC = () => {
+  return (
+    <Block>
+      <Text>image</Text>
+    </Block>
+  );
+};
+export default ImageTab;
---
+++ src/components/profile/status-tab/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Block, Text } from "../../../helper";
+const StatusTab: React.FC = () => {
+  return (
+    <Block>
+      <Text>image</Text>
+    </Block>
+  );
+};
+export default StatusTab;
