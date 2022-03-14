731f8ae03b8a4e9d73263bc9870a2f4ccc7c95ce
taitranjd0904 <tai.tran@enrichco.us>
2022-03-14 14:09:15 +0700

    fix

--- src/components/profile/information/index.tsx
+++ src/components/profile/information/index.tsx
@@ -1,24 +1,29 @@
 import React from "react";
-import { AntIcon, Block, MaIcon, Text } from "../../../helper";
+import { useTranslation } from "react-i18next";
+import { AntIcon, Block, Button, MaIcon, Text } from "../../../helper";
 import Image from "../../../helper/Image";
+import { $primary } from "../../../helper/theme";
 const Information: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <Block>
       <Image pure source={require("../../../assets/images/bg.jpg")} style={{ height: 150 }} />
       <Block>
-        <Image
-          pure
-          source={require("../../../assets/images/Rose.jpg")}
-          style={{
-            height: 100,
-            width: 100,
-            borderRadius: 50,
-            marginTop: -40,
-            marginLeft: 20,
-            borderWidth: 2,
-            borderColor: "white",
-          }}
-        />
+        <Button>
+          <Image
+            pure
+            source={require("../../../assets/images/Rose.jpg")}
+            style={{
+              height: 100,
+              width: 100,
+              borderRadius: 50,
+              marginTop: -40,
+              marginLeft: 20,
+              borderWidth: 2,
+              borderColor: "white",
+            }}
+          />
+        </Button>
         <Text size={20} title center ml={20} style={{ width: 100 }}>
           Tài trần
         </Text>
@@ -39,6 +44,17 @@ const Information: React.FC = () => {
             <Text color="black">Đầu bếp</Text>
           </Block>
         </Block>
+        <Button
+          ma={20}
+          px={15}
+          py={5}
+          radius={5}
+          style={{ position: "absolute", right: 0, backgroundColor: $primary }}
+        >
+          <Text title size={14} color="white">
+            {t("FOLLOW")}
+          </Text>
+        </Button>
       </Block>
     </Block>
   );
